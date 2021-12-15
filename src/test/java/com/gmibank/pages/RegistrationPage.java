5bd3fe50a2c1b3813a87be141e3a97004eef4b9e
mucahit2664 <mucahit2664@gmail.com>
2021-12-15 22:53:01 +0300

    invalid

--- src/test/java/com/gmibank/pages/RegistrationPage.java
+++ src/test/java/com/gmibank/pages/RegistrationPage.java
@@ -1,23 +1,23 @@
 package com.gmibank.pages;
 
 import com.gmibank.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class US01_Pages {
+public class RegistrationPage {
 
-
-    public US01_Pages(){
+    public RegistrationPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
 
     @FindBy(id= "account-menu")
-    public WebElement humanHead;
+    public WebElement loginModule;
 
-    @FindBy(xpath = "(//a[@class='dropdown-item'])[2]")
-    public WebElement registerButonu;
+    @FindBy(xpath = "//span[text()='Register']")
+    public WebElement registerButton;
 
     @FindBy (xpath = "//input[@id='ssn']")
     public WebElement ssnTextBox;
@@ -34,7 +34,7 @@ public class US01_Pages {
     @FindBy (xpath = "//input[@id='mobilephone']")
     public WebElement phoneNumberTextBox;
 
-    @FindBy (xpath = " //input[@id='username']")
+    @FindBy(xpath = " //input[@id='username']")
     public WebElement usernameTextBox;
 
     @FindBy (xpath = "//input[@id='email']")
@@ -47,10 +47,12 @@ public class US01_Pages {
     public WebElement secondPasswordTextBox;
 
     @FindBy (id = "register-submit")
-    public WebElement registerButton;
+    public WebElement registerSubmitField;
 
     @FindBy(xpath = "//div[@style='pointer-events: none;']")
     public WebElement savedMessage;
 
-
+    public WebElement getInvalidFeedback(String message) {
+        return Driver.getDriver().findElement(By.xpath("//div[text()='" + message + "']"));
+    }
 }
